98f8400dd928c158ddd59a900c485f6b5e1df4aa
Alix M <57956860+kyaEH@users.noreply.github.com>
2022-10-31 17:40:27 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+#HTML5 Character Sheet#
+####A character sheet template made with HTML and CSS for a custom roleplaying game.####
+
+###What###
+A homebrew character sheet for a custom roleplaying game designed by me. While built with HTML and styled with vanilla CSS, it's primarily meant for print. In the future, it may evolve into a live interactive one.
+
+###Why###
+I needed to build a custom character sheet for a roleplaying game I've designed. Apart from tools like InDesign, Scribus or Excel, good ol' plain HTML and CSS felt the most natural. It's easy to customize, the history will be here in GitHub and developing it will be good review on CSS basics. Moreover, it'll be easier to make an interactive character sheet in the future.
+
+As a developer I rarely get to do CSS that's actually put into physical form at some point. It'll be a nice venture on that front. Inspired by CSS Secrets, the book by Lea Verou (2015).
+
+*The roleplaying game mentioned above is not public at the moment.*
+
+MIT
---
+++ scripts.js
@@ -0,0 +1,68 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+$(document).ready(function(){
+    $("input").val(""); 
+    $(".trait").val("1"); 
+});
+
+$("input").change(function(){
+    $("#precision").val(Math.min(17,Math.round(
+        $("#agilite").val()/2+$("#dexterite").val()/1.25
+    )));
+
+    $("#degats").val(Math.min(17,Math.round($("#force").val())));
+    $("#crit").val(Math.round(
+        $("#force").val()/2+$("#agilite").val()/2
+        ));
+  }); 
+
+
+
+function sendMessage(stat) {
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/1036662078775234622/tgDzlgPgjfvVxTdvkA74_1xxai6kHqYzizSGeP7nbAwV1VbN72do3JoPHHbKD3GcYzxC");
+
+    request.setRequestHeader('Content-type', 'application/json');
+    var calcul=Math.floor(Math.random()*20+1);
+    var charname = $("#charname").val();
+    if(stat == calcul || calcul == 1){
+        test=JSON.stringify(charname+": 🎲⚠️ - Dé réussi, vous avez fait: "+calcul+" en REUSSITE CRITIQUE!");
+    }
+    else
+    if (calcul<stat && stat!=1 && stat!=calcul){
+        test=JSON.stringify(charname+": 🎲✅ - Dé réussi, vous avez fait: "+calcul);
+    }
+   
+    else {
+        test=JSON.stringify(charname+":🎲❌ - Dé loupé, vous avez fait: "+calcul);
+    }
+    var unquoted= test.replace(/"/g, '');
+    var params = {
+        username: "Lanceur de dé - Faisceau d'Aurora",
+        avatar_url: "https://pbs.twimg.com/profile_images/1477777757732061188/kRVB-Rbl_400x400.jpg",
+        content: unquoted
+    }
+
+    request.send(JSON.stringify(params));
+}
